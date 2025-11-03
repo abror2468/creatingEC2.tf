c9cdd9c65b7fa652d25eea59c5c989416f9c0e49
abror2468 <abror2468@gmail.com>
2025-11-03 12:56:40 -0500

    Update CreatingInstance.tf

--- CreatingInstance.tf
+++ CreatingInstance.tf
@@ -16,30 +16,6 @@ resource "aws_subnet" "GodssecondCreation" {
   cidr_block = "10.0.1./24"
 }
 
-resource "aws_lb_target_group" "test" {
-  name     = "tf-example-lb-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
-}
-resource "aws_lb" "test" {
-  name               = "test-lb-tf"
-  internal           = false
-  load_balancer_type = "application"
-  subnets            = GodssecondCreation.id
-
-  enable_deletion_protection = true
-
-  access_logs {
-    bucket  = aws_s3_bucket.lb_logs.id
-    prefix  = "test-lb"
-    enabled = true
-  }
-
-  tags = {
-    Environment = "production"
-  }
-}
 
 
 provider "aws" {
