8d982679b722597e3bb04bd0e591583ad0fa8836
abror2468 <abror2468@gmail.com>
2025-11-03 13:00:31 -0500

    Update CreatingInstance.tf

--- CreatingInstance.tf
+++ CreatingInstance.tf
@@ -7,7 +7,7 @@ terraform {
   }
 }
 resource "aws_vpc_ipv4_cidr_block_association" "GodsCreation" {
-  vpc_id     = aws_vpc.main.id
+  vpc_id     = aws_vpc.AMEN.id
   cidr_block = "10.0.0.0/16"
 }
 
@@ -16,10 +16,18 @@ resource "aws_subnet" "GodssecondCreation" {
   cidr_block = "10.0.1./24"
 }
 
-
+resource "aws_vpc" "AMEN" {
+  cidr_block = "10.0.0.0/16"
+}
 
 provider "aws" {
   region = "us-east-1"
+}
+
+resource "aws_ec2_subnet_cidr_reservation" "example" {
+  cidr_block       = "10.0.0.16/28"
+  reservation_type = "prefix"
+  subnet_id        = aws_subnet.example.id
 }
 
 resource "aws_instance" "AbrorIsDaGoat" {
