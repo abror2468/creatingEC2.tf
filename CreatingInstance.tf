47b11fecc629259d6d0c04a9d2d650c99f5ba056
abror2468 <abror2468@gmail.com>
2025-11-03 12:55:25 -0500

    Update CreatingInstance.tf

--- CreatingInstance.tf
+++ CreatingInstance.tf
@@ -6,6 +6,41 @@ terraform {
     }
   }
 }
+resource "aws_vpc_ipv4_cidr_block_association" "GodsCreation" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "GodssecondCreation" {
+  vpc_id     = aws_vpc_ipv4_cidr_block_association.GodsCreation.vpc_id
+  cidr_block = "10.0.1./24"
+}
+
+resource "aws_lb_target_group" "test" {
+  name     = "tf-example-lb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+}
+resource "aws_lb" "test" {
+  name               = "test-lb-tf"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = GodssecondCreation.id
+
+  enable_deletion_protection = true
+
+  access_logs {
+    bucket  = aws_s3_bucket.lb_logs.id
+    prefix  = "test-lb"
+    enabled = true
+  }
+
+  tags = {
+    Environment = "production"
+  }
+}
+
 
 provider "aws" {
   region = "us-east-1"
